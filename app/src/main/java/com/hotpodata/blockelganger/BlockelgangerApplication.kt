04f18f7d13e5767036d416422497478cdb27c126
Joel Drotos <jrdrotos@gmail.com>
2016-01-04 09:02:43 -0600

    Setup Application class

---
+++ app/src/main/java/com/hotpodata/blockelganger/BlockelgangerApplication.kt
@@ -0,0 +1,16 @@
+package com.hotpodata.blockelganger
+
+import android.support.multidex.MultiDexApplication
+import timber.log.Timber
+
+/**
+ * Created by jdrotos on 1/4/16.
+ */
+class BlockelgangerApplication : MultiDexApplication() {
+    public override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.LOGGING_ENABLED) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
