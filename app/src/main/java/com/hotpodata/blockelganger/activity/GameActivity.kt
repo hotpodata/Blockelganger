ddbd258b1dfa80fe2dafef3a25285ff44cc50267
Joel Drotos <jrdrotos@gmail.com>
2016-01-04 09:04:59 -0600

    Move activity to its own package and convert to kotlin

---
+++ app/src/main/java/com/hotpodata/blockelganger/activity/GameActivity.kt
@@ -0,0 +1,13 @@
+package com.hotpodata.blockelganger.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hotpodata.blockelganger.R
+
+class GameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
